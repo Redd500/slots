421a13a66a3bb119c91148c380636f0d99362da7
Redd500 <reddfiveohoh@gmail.com>
2017-01-12 21:20:43 -0500

    first commit

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,56 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
 
 @Component({
-  selector: 'my-app',
-  template: `<h1>Hello {{name}}</h1>`,
+	moduleId: module.id,
+	selector: 'my-app',
+	templateUrl: 'app.component.html',
+	styleUrls: [ 'app.component.css' ]
 })
-export class AppComponent  { name = 'Angular'; }
+
+export class AppComponent implements OnInit  {
+	money = 100;
+	
+	square1 = -1;
+	square2 = -1;
+	square3 = -1;
+	
+	bet = 1;
+	
+	changeSquare1(): void {
+		this.square1 = Math.floor(Math.random() * 6);
+	}
+	
+	changeSquare2(): void {
+		this.square2 = Math.floor(Math.random() * 6);
+	}
+	
+	changeSquare3(): void {
+		this.square3 = Math.floor(Math.random() * 6);
+	}
+	
+	rollSlots(): void {
+		this.money -= this.bet;
+		Observable.interval(100).take(15).subscribe(t => {
+			this.changeSquare1();
+		});
+		Observable.interval(100).take(25).subscribe(t => {
+			this.changeSquare2();
+		});
+		Observable.interval(100).take(35).subscribe(t => {
+			this.changeSquare3();
+			if (t == 34) {
+				this.checkWin();
+			}
+		});
+		
+	}
+	
+	checkWin(): void {
+		if (this.square1 == this.square2 && this.square1 == this.square3)
+		{
+			this.money += this.bet * 30;
+		}
+	}
+}
